3eb98d1b3d598e1256c393c618e490b4eb96faed
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2026-02-27 19:37:05 +0530

    create a certificate section

---
+++ src/componants/Certificed.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import C1 from '../images/C1.webp'
+import C2 from '../images/C2.webp'
+import C3 from '../images/C3.webp'
+import C4 from '../images/C4.svg'
+import C5 from '../images/C5.webp'
+import C6 from '../images/C6.webp'
+import C7 from '../images/C6.webp'
+import { GoDotFill } from "react-icons/go";
+
+function Certificed() {
+  return (
+    <div className='certificedSection'>
+      <h1>CERTIFIED & EXCELLENCE</h1>
+      <p>Government recognized and internationally accredited healthcare standards</p>
+      <div className="tagline"><GoDotFill className='dot' /> OFFICIALLY CERTIFIED</div>
+      <div className="certificates_list">
+        <marquee behavior="scroll" direction="left" scrollamount="30" pauseOnHover={true}  >
+
+          <div className="merquee d-flex gap-5" >
+            <div className="certificate">
+              <img src={C1} alt="" style={{ width: "100px" }} />
+              <p>Quality Healthcar</p>
+            </div>
+            <div className="certificate">
+              <img src={C2} alt="" style={{ width: "120px" }} />
+              <p>Quality Healthcar</p>
+            </div>
+            <div className="certificate">
+              <img src={C3} alt="" />
+              <p>Quality Healthcar</p>
+            </div>
+            <div className="certificate">
+              <img src={C4} alt="" />
+              <p>Quality Healthcar</p>
+            </div>
+            <div className="certificate">
+              <img src={C5} alt="" />
+              <p>Quality Healthcar</p>
+            </div>
+            <div className="certificate">
+              <img src={C6} alt="" />
+              <p>Quality Healthcar</p>
+            </div>
+            <div className="certificate">
+              <img src={C7} alt="" />
+              <p>Quality Healthcar</p>
+            </div>
+
+          </div>
+        </marquee>
+      </div>
+    </div>
+
+  )
+}
+
+export default Certificed
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../componants/Navbar.jsx'
 import HeroSection from '../componants/HeroSection.jsx'
+import Certificed from '../componants/Certificed.jsx'
 function Homepage() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <hr style={{color:"#15db96ee", border:"3px solid", padding:"0px"}} />
+      <Certificed />
     </div>
   )
 }
